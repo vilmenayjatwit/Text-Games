358378c7b97d321441e1f970548398ad0dc6f852
vilmenayjatwit <90573112+vilmenayjatwit@users.noreply.github.com>
2025-01-10 17:03:47 -0500

    Testing code

---
+++ CookingMama.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CookingMama {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Hello World"); 
+    }
+}
