117e82337a1395c7fca23efce81c79216b3b32e2
Namachan <nnkmr1126@gmail.com>
2025-01-12 00:36:47 -0500

    AREA IV DONE :)

--- IV.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class IV {
-    public IV() {
-    }
-}
---
+++ src/IV.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IV {
+    public int enter(Scanner scanner, ArrayList<String> visitedAreas, int dishNumber) {
+    	
+    	System.out.println("Area IV: Serve your dish correctly to comple it!");
+    	
+    	
+    	
+    	System.out.println("1) YES");
+    	
+    	System.out.println("2) NO");
+    	
+    	System.out.println("Please type your choice:");
+    	
+    	
+    	
+    	int choice = -1; //Restart the dish
+    	
+    	while (true) {
+    		if (scanner.hasNextInt()) {
+    			choice = scanner.nextInt();
+    			if (choice == 1 || choice == 2) {
+    				break;
+    			}
+    			else {
+    				System.out.println("Invalid choice. Please type 1 for YES or 2 for NO.");
+    			}
+    		}
+    		else {
+    			System.out.println("Invalid input. Please enter a number (1 or 2)!");
+    			n.next();
+    		}
+    	}
+    	
+    	if (choice == 1) {
+    		System.out.println("Dish successfully served! CONGRATULATIONS!");
+    		visitedAreas.add("Dish Completed");
+    		return 5; //returns to main game
+    	}
+    	else {
+    		System.out.println("You chose not to serve the dish yet. STAY IN AREA IV!");
+    		return 4;
+    	}
+    }
+}
